bd00d1a6dc793cb249fa77ef5ca9abf5761fdce5
souhila <souhila.azzouz@monboreal.ca>
2026-04-19 17:38:45 -0400

    Implémentation de l'ajout via la couche service

--- services/services_salle.py
+++ services/services_salle.py
@@ -11,3 +11,9 @@ def ajouter_salle(self, salle):
         return False, "Tous les champs sont obligatoires"
     if  int(salle.capacite) < 1:
         return False, "La capacité doit être supérieure ou égale à 1"
+
+    try:
+        self.dao_salle.insert_salle(salle)
+        return True, "Salle ajoutée avec succès"
+    except Exception as e:
+        return False, str(e)

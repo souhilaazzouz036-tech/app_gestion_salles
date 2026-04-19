98dac82b3462f7e21fa827db98570eb7e0d613d8
souhila <souhila.azzouz@monboreal.ca>
2026-04-19 17:58:46 -0400

    Création de la fenêtre principale customTkinter

---
+++ views/view_salle.py
@@ -0,0 +1,15 @@
+import customtkinter as ctk
+from services.services_salle import ServiceSalle
+
+ctk.set_appearance_mode("light")
+ctk.set_default_color_theme("blue")
+
+
+class ViewSalle(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Gestion des salles")
+        self.geometry("700x500")
+
+        self.service_salle = ServiceSalle()

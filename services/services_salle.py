71d104a0d1beb8c45b5fe6e603436e96a48508ad
souhila <souhila.azzouz@monboreal.ca>
2026-04-19 17:33:59 -0400

    Ajout de la validation des champs dans le service

--- services/services_salle.py
+++ services/services_salle.py
@@ -4,3 +4,8 @@ from Data.dao_salle import DataSalle
 class ServiceSalle:
     def __init__(self):
         self.dao_salle = DataSalle()
+
+
+def ajouter_salle(self, salle):
+    if not salle.code or not salle.libelle or not salle.type or not salle.capacite:
+        return False, "Tous les champs sont obligatoires"

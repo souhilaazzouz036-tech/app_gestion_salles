a98147e045a37862c232eda9e9fdadd8b4926766
souhila <souhila.azzouz@monboreal.ca>
2026-04-19 18:40:19 -0400

    Implémentation de la méthode lister_salles

--- views/view_salle.py
+++ views/view_salle.py
@@ -115,6 +115,13 @@ class ViewSalle(ctk.CTk):
             self.treeList.column("capacite", width=100)
             self.treeList.pack(expand=True, fill="both", padx=10, pady=10)
 
+    def lister_salles(self):
+        self.treeList.delete(*self.treeList.get_children())
+        liste = self.service_salle.recuperer_salles()
+        for s in liste:
+            self.treeList.insert("", "end", values=(s.code, s.libelle, s.type, s.capacite))
+
+
 
 
 

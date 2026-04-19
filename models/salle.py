15bc1ca5e37666f97f06f276662d7ee49a38917d
souhila <souhila.azzouz@monboreal.ca>
2026-04-19 16:46:36 -0400

    Création du modèle Salle

---
+++ models/salle.py
@@ -0,0 +1,6 @@
+class Salle:
+    def __init__(self, code, libelle, type, capacite):
+        self.code = code
+        self.libelle = libelle
+        self.type = type
+        self.capacite = capacite

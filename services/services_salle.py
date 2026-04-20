97e35c593421239a46722de334849001af7a7bfb
souhila <souhila.azzouz@monboreal.ca>
2026-04-19 21:52:34 -0400

    Mise a jour de la couche service

--- services/services_salle.py
+++ services/services_salle.py
@@ -2,51 +2,59 @@ from Data.dao_salle import DataSalle
 from models.salle import Salle
 
 
-
 class ServiceSalle:
     def __init__(self):
         self.dao_salle = DataSalle()
 
+    def ajouter_salle(self, salle):
+        if not salle.code or not salle.libelle or not salle.type or not salle.capacite:
+            return False, "Tous les champs sont obligatoires"
 
-def ajouter_salle(self, salle):
-    if not salle.code or not salle.libelle or not salle.type or not salle.capacite:
-        return False, "Tous les champs sont obligatoires"
-    if  int(salle.capacite) < 1:
-        return False, "La capacité doit être supérieure ou égale à 1"
+        if int(salle.capacite) < 1:
+            return False, "La capacité doit être supérieure ou égale à 1"
 
-    try:
-        self.dao_salle.insert_salle(salle)
-        return True, "Salle ajoutée avec succès"
-    except Exception as e:
-        return False, str(e)
+        try:
+            self.dao_salle.insert_salle(salle)
+            return True, "Salle ajoutée avec succès"
+        except Exception as e:
+            return False, str(e)
 
+    def modifier_salle(self, salle):
+        if not salle.code or not salle.libelle or not salle.type or not salle.capacite:
+            return False, "Tous les champs sont obligatoires"
 
-def supprimer_salle(self, code):
-    try:
-        self.dao_salle.delete_salle(code)
-        return True, "Salle supprimée avec succès"
-    except Exception as e:
-        return False, str(e)
+        if int(salle.capacite) < 1:
+            return False, "La capacité doit être supérieure ou égale à 1"
 
+        try:
+            self.dao_salle.update_salle(salle)
+            return True, "Salle modifiée avec succès"
+        except Exception as e:
+            return False, str(e)
 
-def rechercher_salle(self, code):
-    try:
-        resultat = self.dao_salle.get_salle(code)
-        if resultat:
-            return Salle(resultat[0], resultat[1], resultat[2], resultat[3])
-        return None
-    except Exception as e:
-        print("Erreur recherche :", e)
-        return None
+    def supprimer_salle(self, code):
+        try:
+            self.dao_salle.delete_salle(code)
+            return True, "Salle supprimée avec succès"
+        except Exception as e:
+            return False, str(e)
 
+    def rechercher_salle(self, code):
+        try:
+            resultat = self.dao_salle.get_salle(code)
+            if resultat:
+                return Salle(resultat[0], resultat[1], resultat[2], resultat[3])
+            return None
+        except Exception as e:
+            print("Erreur recherche :", e)
+            return None
 
-def recuperer_salles(self):
-    liste_objets = []
-    try:
-        resultats = self.dao_salle.get_salles()
-        for r in resultats:
-            liste_objets.append(Salle(r[0], r[1], r[2], r[3]))
-    except Exception as e:
-        print("Erreur récupération :", e)
-    return liste_objets
-
+    def recuperer_salles(self):
+        liste_objets = []
+        try:
+            resultats = self.dao_salle.get_salles()
+            for r in resultats:
+                liste_objets.append(Salle(r[0], r[1], r[2], r[3]))
+        except Exception as e:
+            print("Erreur récupération :", e)
+        return liste_objets
